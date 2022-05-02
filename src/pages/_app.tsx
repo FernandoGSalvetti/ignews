84c6cdd7a20b0edcb64f2afa834525e67ca55464
Fernando Salvetti <fernandogsalvetti@gmail.com>
2022-05-02 12:52:07 -0300

    Tela de home

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "../../styles/global.scss";
+import { Header } from "../components/Header/index";
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
